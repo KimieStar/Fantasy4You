a6c269e3ebaf24c9a73218c9550fafed9a08da1e
Petko <64313946+pee4kotoo@users.noreply.github.com>
2022-02-11 22:00:43 +0200

    Builder for obtaining the connection string from appsettings.json

--- DAL/DataContent/AppConfig.cs
+++ DAL/DataContent/AppConfig.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace DAL.DataContent
 {
-    internal class AppConfig
+    public class AppConfig
     {
+        public AppConfig()
+        {
+            ///<summary>
+            ///Използваме Този ConfigurationBuilder 
+            ///за да извлечем connection string от appsettings, 
+            ///който connection string ни е нужен 
+            ///за да осъществим връзка с датабазата
+            /// </summary>
+
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            configurationBuilder.AddJsonFile(path, false);
+            IConfigurationRoot root = configurationBuilder.Build();
+            IConfigurationSection appSettings = root.GetSection("ConnectionStrings:DefaultConnection");
+            SqlConnectionString = appSettings.Value;
+
+
+
+        }
+
+        public string SqlConnectionString { get; set; }
+
     }
 }
